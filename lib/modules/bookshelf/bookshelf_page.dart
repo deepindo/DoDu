8438f68834cd614f7815c5bb9c094b109a9c3a3c
deepindo <732872042@qq.com>
2023-04-19 00:08:26 +0800

    集成  flutter_screenutil: ^5.7.0、shared_preferences: ^2.0.18、pull_to_refresh: ^2.0.0；添加屏幕适配器DoSA

--- lib/modules/bookshelf/bookshelf_page.dart
+++ lib/modules/bookshelf/bookshelf_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class BookshelfPage extends StatefulWidget {
   const BookshelfPage({super.key});
@@ -8,10 +10,48 @@ class BookshelfPage extends StatefulWidget {
 }
 
 class _BookshelfPageState extends State<BookshelfPage> {
+  final double _navAlpha = 0.0;
+  final RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
+  /// Function Syntax
+  ///下拉刷新
+  void _onRefresh() {
+    _refreshController.refreshCompleted();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("书架"),
+    return AnnotatedRegion(
+      value: _navAlpha > 0.5
+          ? SystemUiOverlayStyle.dark
+          : SystemUiOverlayStyle.light,
+      child: Stack(
+        children: [_customContentView(), _customNavigatonBar()],
+      ),
     );
+  }
+
+  /// UI Syntax
+  ///
+  Widget _customNavigatonBar() {
+    return Positioned(child: Stack());
+  }
+
+  Widget _customContentView() {
+    return SmartRefresher(
+        enablePullDown: true,
+        // enablePullUp: true,
+        // enableTwoLevel: true,
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        // onLoading: controller.onLoad,
+        // onTwoLevel: (isOpen) {},
+        // header: ClassicFooter(),
+        header: const WaterDropMaterialHeader(
+          color: Colors.white,
+          // backgroundColor: DoColors.theme,
+        ),
+        child: ListView());
   }
 }
---
+++ lib/utils/do_screen_adapter.dart
@@ -0,0 +1,91 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+///屏幕适配
+class DoSA {
+  ///适配宽
+  static w(num value) {
+    return value.w;
+  }
+
+  ///适配高
+  static h(num value) {
+    return value.h;
+  }
+
+  ///适配宽高中较小一方
+  static r(num value) {
+    return value.r;
+  }
+
+  ///适配字体
+  static fs(num value) {
+    return value.sp;
+  }
+
+  ///屏宽
+  static screenW() {
+    return ScreenUtil().screenWidth; //1.sw
+  }
+
+  ///屏高
+  static screenH() {
+    return ScreenUtil().screenHeight; //1.sh
+  }
+
+  ///状态栏高度 刘海屏会更高
+  static statusH() {
+    return ScreenUtil().statusBarHeight; //1.sh
+  }
+
+  ///对于导航栏(不加状态栏的高)，两端统一用这个
+  ///方便控制
+  static navH() {
+    return 44.h; //1.sh
+  }
+
+  ///对于底部栏(不加安全区的高)，两端统一用这个
+  ///方便控制
+  static tabBarH() {
+    return 49.h; //1.sh
+  }
+
+  ///底部安全区距离，适用于全面屏下面有按键的
+  ///因为安卓手机获取的值为0，实际显示其实是和iOS-x系列以后一样，
+  ///底部是个弧形的屏幕，太贴底，而且感觉按49高度设置的水平居中的控制，实际看起来的效果是偏下
+  ///对于这个，在这控制更加方便
+  static bottomH() {
+    return ScreenUtil().bottomBarHeight != 0.0
+        ? ScreenUtil().bottomBarHeight
+        : 34.0; //1.sh
+  }
+
+  static adapterBottomH() {
+    return tabBarH() + bottomH();
+  }
+}
+
+/**
+ * #define kISPortrait             UIInterfaceOrientationIsPortrait([[UIApplication sharedApplication] statusBarOrientation])   /// 是否是竖屏
+#define kISLandscape            UIInterfaceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation])   /// 是否是横屏
+#define kISPad_Model            (([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) ? YES : NO)
+#define kISPhone_Model          (([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) ? YES : NO)
+#define kISIPhoneX_Serial       ([[UIApplication sharedApplication] statusBarFrame].size.height > 20 ? YES : NO)
+
+#define kSharedApplication      ([UIApplication sharedApplication])             /// 主程序
+#define kSharedWindow           ([UIApplication sharedApplication].keyWindow)   /// 主window
+#define kSharedDelegate         ([UIApplication sharedApplication].delegate)    /// 主delegate
+#define kUserDefaults           ([NSUserDefaults standardUserDefaults])         /// 偏好设置
+#define kScreenBounds           ([UIScreen mainScreen].bounds)   /// 屏幕区域
+#define kScreenSize             ([UIScreen mainScreen].bounds.size)         /// 屏幕尺寸
+#define kScreenWidth            (kISPortrait ? [UIScreen mainScreen].bounds.size.width : [UIScreen mainScreen].bounds.size.height)   /// 屏幕屏宽
+#define kScreenHeight           (kISPortrait ? [UIScreen mainScreen].bounds.size.height : [UIScreen mainScreen].bounds.size.width)   /// 屏幕屏高
+#define kScreenScale            (kScreenWidth / (kISPad_Model ? 810.0 : 375.0))                        /// 屏幕比例---iPad是以8代的竖屏宽来适配的
+#define kUIAdapter(v)           (v*kScreenScale)              /// 尺寸适配
+#define kRectAdapter(x,y,w,h)   (CGRectMake(kUIAdapter(x), kUIAdapter(y), kUIAdapter(w), kUIAdapter(h)))              /// rect适配
+
+#define kNavBarH                (kISIPhoneX_Serial == YES ? 88 : 64)  /// 导航栏高度
+#define kTabBarH                (kISIPhoneX_Serial == YES ? 83 : 49)  /// 底部Tabbar高度
+#define kStatusBarH             (kISIPhoneX_Serial == YES ? 44 : 20)  /// 状态栏高度, 安卓咋是33.0，iOS是47
+#define kSafeAreaBottomHeight   (kISIPhoneX_Serial == YES ? 34 : 0)   /// 底部安全区高度，安卓是0.0，iOS是34
+
+ */
